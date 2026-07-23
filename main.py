70338dbb37cda38ee5283d888c4d7899c38c3fad
Анастасія <anastasiia.doroshok.25@kse.org.ua>
2026-07-23 17:53:59 +0300

    add clear data

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+ticker = "BTC-USD"
+raw_data = yf.download(ticker, start="2023-08-28", end="2026-04-06")
+
+df=pd.DataFrame(raw_data)
+
+#Перевірка даних
+#print(df.isnull().sum())
+df=df.dropna()
+print(df)
