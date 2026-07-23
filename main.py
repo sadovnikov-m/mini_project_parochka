34ee6f83be9627e1c68d083f77b25b233fad7e65
Анастасія <anastasiia.doroshok.25@kse.org.ua>
2026-07-23 19:19:18 +0300

    add P&L

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from markdown_it.rules_inline import balance_pairs
 
 ticker = "BTC-USD"
 raw_data = yf.download(ticker, start="2023-08-28", end="2026-04-06")
@@ -26,4 +27,23 @@ df=df.dropna()
 # print(df['Signal'])
 # print(df['Moment'])
 #Сигнали: 1.0 - точка купівлі, -1.0 - точка продажу
+
+#Розрахуйте прибуток/збиток (P&L)
+balance = 0.0
+buy_price = 0.0
+in_position = False
+for i in range(len(df)):
+    current_price = float(df['Close'].iloc[i].item())
+    moment = float(df['Moment'].iloc[i].item())
+    if moment == 1.0 and not in_position:
+        buy_price = current_price
+        in_position = True
+    elif moment == -1.0 and in_position:
+        sell_price = current_price
+        trade_profit = sell_price - buy_price
+        balance += trade_profit
+        in_position = False
+print(f"Загальний прибуток: {balance:.2f}$")
+
+
 
