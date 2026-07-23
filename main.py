9e82094d7efb48993091faca1015bd9ccf4bf482
sadovnikov-m <mykhailo.sadovnikov.25@kse.org.ua>
2026-07-23 18:47:11 +0300

    add trading signals

--- main.py
+++ main.py
@@ -11,4 +11,19 @@ df=pd.DataFrame(raw_data)
 #Перевірка даних
 #print(df.isnull().sum())
 df=df.dropna()
-print(df)
+
+# Розробка базової моделі торгівлі на основі простої стратегії.
+short_window=30
+long_window=60
+
+df['SMA_short']=df['Close'].rolling(window=short_window).mean()
+df['SMA_long']=df['Close'].rolling(window=long_window).mean()
+
+df['Signal']=0.0
+df['Signal']=np.where(df['SMA_short']>df['SMA_long'],1.0,0.0)
+df['Moment']=df['Signal'].diff()
+df=df.dropna()
+# print(df['Signal'])
+# print(df['Moment'])
+#Сигнали: 1.0 - точка купівлі, -1.0 - точка продажу
+
